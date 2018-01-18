9e560c5317d0b1374d52be6f2b884029e6bc39c6
Meggie Grimaud <meggie.grimaud@misys.com>
2018-01-18 15:34:58 +0100

    add Python payoffs

--- CustomPayoff/run.py
+++ CustomPayoff/run.py
@@ -2,7 +2,20 @@ import os
 import json
 import math
 
-def geometricAverage(strike, spots, callOrPut):
+
+def customPayoffPrice(payoffName, postreqdata):
+	if(payOffName == "asian-average"):
+		return arithmeticAverage(postreqdata['strike'], postreqdata['paths'], postreqdata['optionType'])	
+	elif(payOffName == "geometric-average"):
+		return geometricAverage(postreqdata['strike'], postreqdata['paths'], postreqdata['optionType'])
+	elif(payoffName == "oneTouch"):
+		return geometricAverage(postreqdata['strike'], postreqdata['paths'], postreqdata['optionType'])
+	else:
+		print("{} is not supported, please create our function".format(payoffName))
+		return [0.0] * len(postreqdata['paths'])
+
+
+def geometricAverage(strike, spots, optionType):
 	nbPaths = len(spots)
 	sum = [1.0] * nbPaths
 	for j in range(0, nbPaths):
@@ -10,10 +23,11 @@ def geometricAverage(strike, spots, callOrPut):
 		sumTemp = 1.0
 		for i in range(0, nbDates):
 			sumTemp = sumTemp * spots[j][i]
-		sum[j] = max(callOrPut * (math.pow(sumTemp, 1/nbDates) - strike), 0.0)
+		sum[j] = max(optionType * (math.pow(sumTemp, 1/nbDates) - strike), 0.0)
 	return sum
 
-def arithmeticAverage(strike, spots, callOrPut):
+
+def arithmeticAverage(strike, spots, optionType):
 	nbPaths = len(spots)
 	sum = [0.0] * nbPaths
 	for j in range(0, nbPaths):
@@ -21,16 +35,22 @@ def arithmeticAverage(strike, spots, callOrPut):
 		sumTemp = 0.0
 		for i in range(0, nbDates):
 			sumTemp = sumTemp + spots[j][i]
-		sum[j] = max(callOrPut * (sumTemp / nbDates - strike), 0.0)
+		sum[j] = max(optionType * (sumTemp / nbDates - strike), 0.0)
 	return sum
 
 
+def oneTouch(strike, spots):
+	nbPaths = len(spots)
+	sum = [0.0] * nbPaths
+	for j in range(0, nbPaths):
+		if(spots[len(spots[j])] >= strike):
+			sum[j] = 1
+	return sum
 
 
 postreqdata = json.loads(open(os.environ['req']).read())
 response = open(os.environ['res'], 'w')
-payoffList = arithmeticAverage(postreqdata['strike'], postreqdata['paths'], postreqdata['direction'])
-response.write(str(payoffList))
+response.write(str(customPayoffPrice(payoffName, postreqdata)))
 response.close()
 
 
